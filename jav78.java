887eb898f31249db8c505765298515f4e05d4783
Jiss devra <96423636+jissdevra2003@users.noreply.github.com>
2024-05-25 18:42:11 +0530

    Add files via upload

---
+++ jav78.java
@@ -0,0 +1,15 @@
+interface aaa
+{
+public void sam(int e);
+}
+class jav78
+{
+public static void main(String gg[])
+{
+aaa a;
+a=(p)->{ System.out.println(p*p); };
+a.sam(2);
+a=(p)->System.out.println(p*p);
+a.sam(6);
+}
+}
