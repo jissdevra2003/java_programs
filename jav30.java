ddab994bd860e9c4352769258c8ab72c0475b32a
Jiss devra <96423636+jissdevra2003@users.noreply.github.com>
2024-05-25 22:59:04 +0530

    Add files via upload

---
+++ jav30.java
@@ -0,0 +1,16 @@
+class jav30
+{
+public static void main(String gg[])
+{
+String g;
+g="Hi jiss";
+System.out.println(g);
+String m;
+m="hi jiss";
+System.out.println(m);
+String r=new String("Hi jiss");
+System.out.println(r);
+if(g==r) System.out.println("Same");
+else System.out.println("Not same");
+}
+}
